331fb6138c0627ab2837774f4fbc9fbc709cc59b
Aidan Macdonald <aidan.plenert.macdonald@gmail.com>
2020-06-30 16:00:30 -0700

    Basic functionality working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Form from "@rjsf/core";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 function App() {
+  const [schema, setSchema] = useState({});
+  const [verbs, setVerbs] = useState({});
+  const [verb, setVerb] = useState(undefined);
+  const randomVerb = () => {
+    const v = Object.keys(verbs);
+    setVerb(v[Math.floor(Math.random() * v.length)]);
+  }
+
+  const validate = (formData, errors) => {
+    const expected = verbs[verb];
+
+    Object.keys(expected).forEach(
+      tense => {
+        Object.keys(expected[tense]).forEach(
+          pronoun => {
+            if (expected[tense][pronoun] != formData[tense][pronoun]) {
+              errors[tense][pronoun].addError("Not correct: " + expected[tense][pronoun]);
+            }
+          }
+        )
+      }
+    )
+
+    return errors;
+  }
+
+  useEffect(
+    () => {
+      fetch("/verbs.schema.json")
+        .then(res => res.json())
+        .then(res => setSchema(res))
+    }, []
+  );
+
+  useEffect(
+    () => {
+      fetch("/verbs.json")
+        .then(res => res.json())
+        .then(res => setVerbs(res))
+    }, []
+  );
+
+  useEffect(randomVerb, [verbs]);
+  
+  useEffect(
+    () => {
+      setSchema({title: verb, ...schema})
+    }, [verb]
+  )
+
   return (
     <div>
+      <Container>
+        <Row>
+          <Form
+            schema={schema} 
+            validate={validate}
+            ErrorList={() => { return <div></div>; }}
+          />
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from 'react-bootstrap/Navbar';
+
 ReactDOM.render(
   <React.StrictMode>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand>Donne with Finnish</Navbar.Brand>
+    </Navbar>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
