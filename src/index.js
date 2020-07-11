cd9575d913edcf4cd085554f383f27942eed1e64
Aidan Macdonald <aidan.plenert.macdonald@gmail.com>
2020-07-11 11:10:10 -0700

    Setting up React Router and nouns schema

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Route, Link } from "react-router-dom";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand>Donne with Finnish</Navbar.Brand>
-    </Navbar>
-    <App schema="verbs.schema.json" list="verbs.json" />
+    <HashRouter basename="/">
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Nav className="mr-auto">
+            <Navbar.Brand>Donne with Finnish</Navbar.Brand>
+            <Nav.Link href={`${process.env.PUBLIC_URL}#/`}>Verbs</Nav.Link>
+            <Nav.Link href={`${process.env.PUBLIC_URL}#/nouns`}>Nouns</Nav.Link>
+          </Nav>
+        </Navbar>
+        <Route exact path="/">
+          <App schema="verbs.schema.json" list="verbs.json" />
+        </Route>
+        <Route exact path="/nouns">
+          <App schema="nouns.schema.json" list="nouns.json" />
+        </Route>
+      </div>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
